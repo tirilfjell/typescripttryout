f4034ab2c93b8eec9a504728a6a1e5708df63bb5
Tiril Fjell <tirilfjell@Tirils-MacBook-Pro.local>
2021-09-09 16:10:18 +0200

    setup

---
+++ src/TextField.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+    text: string
+}
+
+export const TextField: React.FC<Props> = () => {
+    return(
+        <div>
+            <input type="text" />
+        </div>
+    )
+}
+
