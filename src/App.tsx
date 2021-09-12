142e5b6a5d6b979e5ab23b4a11538d07ba9b45d4
Tiril Fjell <tirilfjell@Tirils-MacBook-Pro.local>
2021-09-12 19:19:45 +0200

    new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { count } from 'console';
 import React from 'react';
+import { Counter } from './Counter';
 import { TextField } from './TextField';
 
 
 const App: React.FC = () => {
-  return <div>
-    <TextField text='heihei' />
+  return (
+  <div>
+  <Counter>
+    {({count, setCount}) => (
+    <div>
+      {count}
+    <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+    )}
+  </Counter>
   </div>
+  )
 }
+
 
 export default App;
--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+
+interface Person {
+    firstName: string
+    lastName: string
+}
+
 
 interface Props {
     text: string
+    ok?: boolean
+    i?: number
+    fn?: (bob: string) => string
+    person: Person
+    handleChange: (event: React.ChangeEventHandler<HTMLInputElement>) => void
 }
 
-export const TextField: React.FC<Props> = () => {
-    return(
+interface TextNode {
+    text: string
+}
+
+
+export const TextField: React.FC<Props> = ({handleChange}) => {
+    const [count, setCount] = useState<TextNode>({ text: 'hello' });
+    const inputRef = useRef<HTMLInputElement>(null);
+    const divRef = useRef<HTMLDivElement>(null)
+
+
+    return (
         <div>
-            <input type="text" />
+            <div ref={divRef}>
+            <input ref={inputRef} onChange={handleChange} />
+            </div>
         </div>
     )
 }
